4b7c6608830be00eba7e07e11cebdd519eb8fc80
HoP-adamas <bh.evaporation6174@gmail.com>
2021-06-04 15:41:21 +0900

    change tree to 0-indexed and modify query

--- SegmentTree.py
+++ SegmentTree.py
@@ -1,47 +1,53 @@
 class SegmentTree:
-    def __init__(self,n,ele=10**10):
+    def __init__(self,n, ele=10**10):
+        '''
+        n: the number of element
+        ele: unit element
+        '''
 
         self.ide_ele = ele
         self.n = n
-        self.num =2**(n-1).bit_length()
+        self.num = 1 << (n-1).bit_length()
         self.seg=[self.ide_ele]*2*self.num
 
 
     #####segfunc######
+
     def segfunc(self,x,y):
-        return min(x,y)
+        '''
+        define a binary operation on the segment tree
+        '''
+        return min(x, y)
 
     def init(self,init_val):
         #set_val
         for i in range(self.n):
-            self.seg[i+self.num-1]=init_val[i]    
+            self.seg[i+self.num]=init_val[i]    
         #built
-        for i in range(self.num-2,-1,-1) :
-            self.seg[i]=self.segfunc(self.seg[2*i+1],self.seg[2*i+2]) 
+        for i in range(self.num-1, 0,-1) :
+            self.seg[i]=self.segfunc(self.seg[2*i],self.seg[2*i+1]) 
         
     def update(self,k,x):
-        k += self.num-1
+        k += self.num
         self.seg[k] = x
-        while k:
-            k = (k-1)//2
-            self.seg[k] = self.segfunc(self.seg[k*2+1],self.seg[k*2+2])
+        while k > 1:
+            k >>= 1
+            self.seg[k] = self.segfunc(self.seg[k*2],self.seg[k*2+1])
         
     def query(self,p,q):
         if q<=p:
             return self.ide_ele
-        p += self.num-1
-        q += self.num-2
+        p += self.num
+        q += self.num
         res=self.ide_ele
-        while q-p>1:
-            if p&1 == 0:
+        while q > p:
+            if p&1:
                 res = self.segfunc(res,self.seg[p])
-            if q&1 == 1:
-                res = self.segfunc(res,self.seg[q])
+                p += 1
+            if q&1:
                 q -= 1
-            p = p//2
-            q = (q-1)//2
-        if p == q:
-            res = self.segfunc(res,self.seg[p])
-        else:
-            res = self.segfunc(self.segfunc(res,self.seg[p]),self.seg[q])
+                res = self.segfunc(res,self.seg[q])
+            p >>= 1
+            q >>= 1
+
         return res

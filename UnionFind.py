daaefb3afacc7beee26c9d09b6f67f496104e6ec
HoP-adamas <bh.evaporation6174@gmail.com>
2021-12-29 21:10:17 +0900

    add a method groups()

--- UnionFind.py
+++ UnionFind.py
@@ -1,36 +1,42 @@
 # Union-Find tree
 
 class UnionFind:
-    def __init__(self, n):
-        # 負  : 根であることを示す。絶対値は木の要素数
-        # 非負: 根でないことを示す。値は親を示す
-        self.table = [-1] * n
+	def __init__(self, n):
+		# 負  : 根であることを示す。絶対値は木の要素数
+		# 非負: 根でないことを示す。値は親を示す
+		self.table = [-1] * n
  
-    def _root(self, x):
-        stack = []
-        tbl = self.table
-        while tbl[x] >= 0:
-            stack.append(x)
-            x = tbl[x]
-        for y in stack:
-            tbl[y] = x
-        return x
- 
-    def find(self, x, y):
-        return self._root(x) == self._root(y)
- 
-    def union(self, x, y):
-        r1 = self._root(x)
-        r2 = self._root(y)
-        if r1 == r2:
-            return
-        if self.table[r1] > self.table[r2]:
-            r1, r2  = r2, r1
-        self.table[r1] += self.table[r2]
-        self.table[r2] = r1
-      
-    def size(self, x):
-      return -self.table[self._root(x)]
+	def _root(self, x):
+		stack = []
+		tbl = self.table
+		while tbl[x] >= 0:
+			stack.append(x)
+			x = tbl[x]
+		for y in stack:
+			tbl[y] = x
+		return x
+
+	def find(self, x, y):
+		return self._root(x) == self._root(y)
+	
+	def union(self, x, y):
+		r1 = self._root(x)
+		r2 = self._root(y)
+		if r1 == r2:
+			return
+		if self.table[r1] > self.table[r2]:
+			r1, r2  = r2, r1
+		self.table[r1] += self.table[r2]
+		self.table[r2] = r1
+
+	def size(self, x):
+		return -self.table[self._root(x)]
+
+	def groups(self):
+		G = [[] for _ in range(len(self.n))]
+		for i in range(self.n):
+			G[self.root(i)].append(i)
+		return [g for g in G if g]
 
 def get_uft_size(uft):
   '''

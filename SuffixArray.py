083921a1d5d2d6fbb28688ba238d2c0d1c412344
HoP-adamas <bh.evaporation6174@gmail.com>
2024-09-14 16:09:20 +0900

    Fix bug

--- SuffixArray.py
+++ SuffixArray.py
@@ -1,12 +1,15 @@
-'''
+"""
 suffix array with SA_IS.
 
 references:
 https://github.com/atcoder/ac-library/blob/master/atcoder/string.hpp
 https://speakerdeck.com/flare/sa-is?slide=61
-'''
+"""
 
 from functools import cmp_to_key
+import typing
+
+
 def sa_naive(s):
     n = len(s)
     sa = [i for i in range(n)]
@@ -20,24 +23,27 @@ def sa_naive(s):
             l += 1
             r += 1
         return -1 if l == n else 1
-    
+
     sa.sort(key=cmp_to_key(cmp))
     return sa
+
 
 def sa_doubling(s):
     n = len(s)
     sa = [i for i in range(n)]
-    rnk = s
+    rnk = s.copy()
     tmp = [0] * n
     k = 1
     while k < n:
+
         def cmp(x, y):
             if rnk[x] != rnk[y]:
                 return -1 if rnk[x] < rnk[y] else 1
-            
+
             rx = rnk[x + k] if x + k < n else -1
             ry = rnk[y + k] if y + k < n else -1
             return -1 if rx < ry else 1
+
         sa.sort(key=cmp_to_key(cmp))
         tmp[sa[0]] = 0
         for i in range(1, n):
@@ -46,19 +52,20 @@ def sa_doubling(s):
         k *= 2
     return sa
 
+
 def sa_is(s, upper):
     THRESHOLD_NAIVE = 10
     THRESHOLD_DOUBLING = 40
     n = len(s)
     if n == 0:
-        return {}
+        return []
     if n == 1:
-        return {0}
+        return [0]
     if n == 2:
         if s[0] < s[1]:
-            return {0, 1}
+            return [0, 1]
         else:
-            return {1, 0}
+            return [1, 0]
 
     if n < THRESHOLD_NAIVE:
         return sa_naive(s)
@@ -66,9 +73,9 @@ def sa_is(s, upper):
         return sa_doubling(s)
 
     sa = [0] * n
-    ls = [False] * n    # if s[i] == S -> ls[i] = True
-    for i in range(n-2, -1, -1):
-        ls[i] = ls[i + 1] if s[i] == s[i+1] else (s[i] < s[i + 1])
+    ls = [False] * n  # if s[i] == S -> ls[i] = True
+    for i in range(n - 2, -1, -1):
+        ls[i] = ls[i + 1] if s[i] == s[i + 1] else (s[i] < s[i + 1])
     sum_l = [0] * (upper + 1)
     sum_s = [0] * (upper + 1)
     for i in range(n):
@@ -76,13 +83,13 @@ def sa_is(s, upper):
             sum_s[s[i]] += 1
         else:
             sum_l[s[i] + 1] += 1
-    
     for i in range(upper + 1):
         sum_s[i] += sum_l[i]
         if i < upper:
-            sum[i + 1]  += sum_s[i + 1]
+            sum_l[i + 1] += sum_s[i]
 
     def induce(lms):
+        nonlocal sa
         for i in range(n):
             sa[i] = -1
         buf = sum_s[:]
@@ -90,7 +97,7 @@ def sa_is(s, upper):
             if d == n:
                 continue
             sa[buf[s[d]]] = d
-            buf[s[[d]]] += 1
+            buf[s[d]] += 1
         buf = sum_l[:]
         sa[buf[s[n - 1]]] = n - 1
         buf[s[n - 1]] += 1
@@ -100,12 +107,12 @@ def sa_is(s, upper):
                 sa[buf[s[v - 1]]] = v - 1
                 buf[s[v - 1]] += 1
         buf = sum_l[:]
-        for i in range(n - 1, -1 -1):
+        for i in range(n - 1, -1, -1):
             v = sa[i]
             if v >= 1 and ls[v - 1]:
                 buf[s[v - 1] + 1] -= 1
                 sa[buf[s[v - 1] + 1]] = v - 1
-    
+
     lms_map = [-1] * (n + 1)
     m = 0
     for i in range(1, n):
@@ -132,17 +139,17 @@ def sa_is(s, upper):
             end_l = lms[lms_map[l] + 1] if lms_map[l] + 1 < m else n
             end_r = lms[lms_map[r] + 1] if lms_map[r] + 1 < m else n
             same = True
-            if end_l - 1 != end_r - r:
+            if end_l - l != end_r - r:
                 same = False
             else:
                 while l < end_l:
-                    if s[i] != s[r]:
+                    if s[l] != s[r]:
                         break
                     l += 1
                     r += 1
                 if l == n or s[l] != s[r]:
                     same = False
-            
+
             if not same:
                 rec_upper += 1
             rec_s[lms_map[sorted_lms[i]]] = rec_upper
@@ -152,15 +159,43 @@ def sa_is(s, upper):
         induce(sorted_lms)
     return sa
 
-def suffix_array(s):
-    return sa_is(list(map(ord, s)), 255)
+
+def suffix_array(s, upper=None):
+    if isinstance(s, str):
+        return sa_is(list(map(ord, s)), 255)
+    elif upper is None:
+        n = len(s)
+        idx = list(range(n))
+
+        def cmp(left, right):
+            return typing.cast(int, s[left]) - typing.cast(int, s[right])
+
+        idx.sort(key=cmp_to_key(cmp))
+        s2 = [0] * n
+        now = 0
+        for i in range(n):
+            if i and s[idx[i - 1]] != s[idx[i]]:
+                now += 1
+            s2[idx[i]] = now
+        return sa_is(s2, now)
+    else:
+        assert 0 <= upper
+        for d in s:
+            assert 0 <= d <= upper
+        return sa_is(s, upper)
+
 
 def lcp_array(s, sa):
+
+    if isinstance(s, str):
+        s = [ord(c) for c in s]
     n = len(s)
     assert n >= 1
+
     rnk = [0] * n
     for i in range(n):
         rnk[sa[i]] = i
+
     lcp = [0] * (n - 1)
     h = 0
     for i in range(n):
@@ -174,4 +209,5 @@ def lcp_array(s, sa):
                 break
             h += 1
         lcp[rnk[i] - 1] = h
+
     return lcp
